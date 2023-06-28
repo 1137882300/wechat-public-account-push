f99eef1af5e8ee07162f721b60448d4f08c57f66
Michael <44225966+1137882300@users.noreply.github.com>
2023-06-28 11:20:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
   USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx1d2cd4c915d14058',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'bf4f06b4164ca1febe3f4cf57106c3b1',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '9ca5f2e3e428403fb932f3bec6ebd783',
+      id: 'oVn-A6QnrnM-1vkMe85LgldUchT8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'tl2SDd8FTm9gzjRamLRD3dztPNQtXgP_JSZtWt6UOvA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
